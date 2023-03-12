54cd329e3a10582fa75ef10d49b4b4656d38bb90
mdm-code <code.madamczyk@gmail.com>
2023-03-12 12:51:04 +0100

    Reflect changes in code in the main command

--- cmd/duct/main.go
+++ cmd/duct/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/mdm-code/duct"
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/mdm-code/duct"
+)
 
 func main() {
-	duct.Do()
+	f, err := duct.NewTempf()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create a temporary files: %s", err)
+	}
+	fds := duct.Fds{
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: io.Discard,
+		Tempf:  f,
+	}
+	cmd := os.Args[1]
+	err = duct.Wrap(cmd, &fds)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to invoke %s: %s", cmd, err)
+	}
 }
