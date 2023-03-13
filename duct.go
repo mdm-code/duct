2db6a9676c9f9f0c8d249f5e5c2c8090220c8dbe
mdm-code <code.madamczyk@gmail.com>
2023-03-13 17:23:36 +0100

    Write up duct package rationale

--- duct.go
+++ duct.go
@@ -1,8 +1,15 @@
 /*
-Package duct provides the internals for the duct program so that it's able to
-wrap a code or text formatter that takes file names as positional arguments
-inside of standard Unix STDIN to STDOUT filter-like data flow. It does so with
-intermediate read/write operations on a temporary file.
+Package duct provides the internals for the duct command-line program wrapping
+code formatters that do not read from standard input data stream, and instead
+they take file names as command arguments. The package offers components that
+allow such commands to be wrapped inside of a standard Unix STDIN to STDOUT
+filter-like data flow.
+
+The general idea is that input data read from standard input is written to an
+intermediate temporary file. The name of the file gets passed to as one of the
+positional arguments of the named program to be executed. The modified contents
+of the file are then re-read and written out the standard output. This way the
+wrapped program can be used as a regular Unix filter.
 */
 package duct
 
