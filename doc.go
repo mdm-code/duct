f843bb36afd848312938d5e990d6dda86e4d659d
mdm-code <code.madamczyk@gmail.com>
2023-11-27 22:34:20 +0100

    Modified package documentation on WrapWrite func

--- doc.go
+++ doc.go
@@ -10,5 +10,10 @@ intermediate temporary file. The name of the file gets passed to as one of the
 positional arguments of the named program to be executed. The modified contents
 of the file are then re-read and written out the standard output. This way the
 wrapped program can be used as a regular Unix filter.
+
+Some code formatters take file names but do not modify files directly. Instead
+they write the formatted code to stdout or stderr. This scenario is supported
+by a the WrapWrite function that writes code from stdin to the temporary file
+but relies on the command's own stdout and/or stderr to write out the output.
 */
 package duct
